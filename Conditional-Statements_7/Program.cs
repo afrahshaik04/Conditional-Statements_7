7ac96dc56717f9c0f5c8fc1896bb96e3cd2b7297
Afrah <afrahshaik@usf.edu>
2024-02-27 12:14:21 -0500

    new commit

--- Conditional-Statements_7/Program.cs
+++ Conditional-Statements_7/Program.cs
@@ -14,7 +14,7 @@ Console.WriteLine("Please enter a Sales Amount ...");
     If the user provides bad input, the catch block
     will handle the error and a message will be displayed.
 */
-//comment
+//comment1
 try
 {
     // This variable will gather the user input
